70abdf2897024272a6a64ffe682433910749b159
Ethan Archer <ethanarcher1@gmail.com>
2022-12-03 13:49:47 +0000

    Day02

--- day01.py
+++ day01.py
@@ -1,25 +1,28 @@
 from aoc import read_input
 
 
-def part1():
-    data = read_input("day01", str)
-    this_elf = 0
-    calorie_list = []
-
-    for v in data:
-        if v != "":
-            this_elf += int(v)
-        else:
-            calorie_list.append(this_elf);
-            this_elf = 0
-
-    calorie_list.sort(reverse=True)
-    print(calorie_list)
-
+def part1(calorie_list):
     print(calorie_list[0])
-    print(calorie_list[1])
-    print(calorie_list[2])
+    return calorie_list[0]
+
+
+def part2(calorie_list):
     print(calorie_list[0] + calorie_list[1] + calorie_list[2])
+    return calorie_list[0] + calorie_list[1] + calorie_list[2]
 
 
-part1()
+data = read_input("day01", str)
+this_elf = 0
+calorie_list = []
+
+for v in data:
+    if v != "":
+        this_elf += int(v)
+    else:
+        calorie_list.append(this_elf)
+        this_elf = 0
+
+calorie_list.sort(reverse=True)
+
+assert (part1(calorie_list) == 70698)
+assert (part2(calorie_list) == 206643)
---
+++ day02.py
@@ -0,0 +1,73 @@
+from aoc import read_input
+
+
+def calculate_score(line):
+    hand = line.split(" ")
+    score = 0
+    if hand[0] == hand[1]: score += 3
+
+    if hand[0] == "A" and hand[1] == "B": score += 6
+    if hand[0] == "B" and hand[1] == "C": score += 6
+    if hand[0] == "C" and hand[1] == "A": score += 6
+
+    if hand[1] == "A": score += 1
+    if hand[1] == "B": score += 2
+    if hand[1] == "C": score += 3
+
+    return score
+
+
+def part1():
+    score_by_hand = []
+
+    for line in data:
+        line = line.replace("X", "A")
+        line = line.replace("Y", "B")
+        line = line.replace("Z", "C")
+        score_by_hand.append(calculate_score(line))
+
+    total_score = sum(score_by_hand)
+    print(total_score)
+    return total_score
+
+
+# X = lost, Y = draw, Z = win
+# A for Rock, B for Paper, and C for Scissors
+def find_hand(line, win):
+    hand = line.split(" ")
+
+    if hand[0] == "A":
+        response = "B" if win else "C"
+        line = line.replace(hand[1], response)
+    elif hand[0] == "B":
+        response = "C" if win else "A"
+        line = line.replace(hand[1], response)
+    elif hand[0] == "C":
+        response = "A" if win else "B"
+        line = line.replace(hand[1], response)
+
+    return line
+
+
+def part2():
+    score_by_hand = []
+
+    for line in data:
+        hand = line.split(" ")
+        if hand[1] == "X":
+            line = find_hand(line, False)
+        elif hand[1] == "Y":
+            line = line.replace("Y", hand[0])
+        elif hand[1] == "Z":
+            line = find_hand(line, True)
+
+        score_by_hand.append(calculate_score(line))
+
+    total_score = sum(score_by_hand)
+    print(total_score)
+    return total_score
+
+
+data = read_input("day02", str)
+assert (part1() == 12679)
+assert (part2() == 14470)
