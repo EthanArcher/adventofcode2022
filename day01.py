b181b9234af628d68917728949a3f7b9fd114238
Ethan Archer <ethanarcher1@gmail.com>
2022-12-03 13:49:47 +0000

    Day01 tidy

--- day01.py
+++ day01.py
@@ -3,31 +3,23 @@ from aoc import read_input
 
 def part1():
     data = read_input("day01", str)
-    data.append("")
     this_elf = 0
-    top1 = 0
-    top2 = 0
-    top3 = 0
+    calorie_list = []
 
     for v in data:
         if v != "":
             this_elf += int(v)
         else:
-            if this_elf > top1:
-                top3 = top2
-                top2 = top1
-                top1 = this_elf
-            elif this_elf > top2:
-                top3 = top2
-                top2 = this_elf
-            elif this_elf > top3:
-                top3 = this_elf
+            calorie_list.append(this_elf);
             this_elf = 0
 
-    print(top1)
-    print(top2)
-    print(top3)
-    print(top1 + top2 + top3)
+    calorie_list.sort(reverse=True)
+    print(calorie_list)
+
+    print(calorie_list[0])
+    print(calorie_list[1])
+    print(calorie_list[2])
+    print(calorie_list[0] + calorie_list[1] + calorie_list[2])
 
 
 part1()
